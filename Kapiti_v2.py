f57b1b0b3e8c836049f968f6df993c566ea72929
Pradelson Francois <81245639+pradelson95@users.noreply.github.com>
2022-12-09 21:03:18 -0400

    Add files via upload

---
+++ Kapiti_v2.py
@@ -0,0 +1,36 @@
+import time
+from googlesearch import search
+from colorama import Fore
+
+print('\33[91m'"""
+                    _wr""        "-q__                             
+                 _dP                 9m_     
+               _#P                     9#_                         
+              d#@                       9#m                        
+             d##                         ###                       
+            J###                         ###L                      
+            {###K                       J###K                      
+            ]####K      ___aaa___      J####F                      
+        __gmM######_  w#P""   ""9#m  _d#####Mmw__                  
+     _g##############mZ_         __g##############m_               
+   _d####M@PPPP@@M#######Mmp gm#########@@PPP9@M####m_             
+  a###""          ,Z"#####@" '######"\g          ""M##m            
+ J#@"             0L  "*##     ##@"  J#              *#K           
+ #"               `#    "_gmwgm_~    dF               `#_          
+7F                 "#_   ]#####F   _dK                 JE          
+]                    *m__ ##### __g@"                   F          
+                       "PJ#####LP"                                 
+ `                       0######_                      '           
+                       _0########_                                   
+     .               _d#####^#####m__              ,              
+      "*w_________am#####P"   ~9#####mw_________w*"                  
+          ""9@#####@M""           ""P@#####@M""                    
+""")
+
+
+Search = input("What do you wanna search: ")
+results = int(input("How many results do you want: "))
+
+for url in search(query=Search, tld="com", num=results, verify_ssl=True, lang="es"):
+    print(url)
+    time.sleep(2)

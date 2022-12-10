c3d3cfe538f2dc1feacf84059e0618c8c7d4b70b
Pradelson Francois <81245639+pradelson95@users.noreply.github.com>
2022-12-09 21:34:27 -0400

    Add files via upload

--- Kapiti_v2.py
+++ Kapiti_v2.py
@@ -1,4 +1,4 @@
-import time
+from time import sleep
 from googlesearch import search
 from colorama import Fore
 
@@ -27,10 +27,15 @@ print('\33[91m'"""
           ""9@#####@M""           ""P@#####@M""                    
 """)
 
+try:
+    Search = input("Que desea buscar?: ")
+    results = int(input("Cuantos resultados desea obtener?: "))
+    print(Fore.YELLOW + "Buscando", Search, "en Google..." + "\n")
+    sleep(2)
 
-Search = input("What do you wanna search: ")
-results = int(input("How many results do you want: "))
+    for url in search(query=Search, tld="com", stop=results, num=results, verify_ssl=True, lang="es"):
+        print(Fore.YELLOW + "enlace: " + url)
+        sleep(2)
 
-for url in search(query=Search, tld="com", num=results, verify_ssl=True, lang="es"):
-    print(url)
-    time.sleep(2)
+except Exception as error:
+    pass

bdc2b9284789cfc62eb4e35c39c29ad0ab808d91
Hugo Mamani <mamaniwilliam383@gmail.com>
2022-12-09 21:23:27 -0500

    changed in Kapiti_v2.py and Banner.py

--- Kapiti_v2.py
+++ Kapiti_v2.py
@@ -1,4 +1,5 @@
 from time import sleep
+from spanlp.palabrota import Palabrota
 from googlesearch import search
 from colorama import Fore
 from Banner import logo
@@ -8,6 +9,10 @@ try:
     results = int(input("Cuantos resultados desea obtener?: "))
     print(Fore.YELLOW + "Buscando", Search, "en Google..." + "\n")
     sleep(2)
+
+    if Palabrota().contains_palabrota(Search):
+        print("\033[91m! No puedes realizar busquedas explicitas")
+        exit(204)
 
     for url in search(query=Search, tld="com", stop=results, num=results, verify_ssl=True, lang="es"):
         print(Fore.YELLOW + "enlace: " + url)
